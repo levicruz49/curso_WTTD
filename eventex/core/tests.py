42888d13f837f1f042ee0deae5baaa81bda0d4d0
Levi Cruz <levicruz49@gmail.com>
2020-06-01 10:27:08 -0300

    habilita inscricao

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -4,12 +4,15 @@ from django.test import TestCase
 
 class HomeTest(TestCase):
     def setUp(self):
-        self.response = self.client.get('/')
+        self.resp = self.client.get('/')
 
     def test_get(self):
         """ GET / MUST RETURN STATUS CODE 200 """
-        self.assertEqual(200, self.response.status_code)
+        self.assertEqual(200, self.resp.status_code)
 
     def test_template(self):
         """ MUST USE INDEX.HTML """
-        self.assertTemplateUsed(self.response, 'index.html')
+        self.assertTemplateUsed(self.resp, 'index.html')
+
+    def test_subscription_lin(self):
+        self.assertContains(self.resp, 'href="/inscricao/"')
