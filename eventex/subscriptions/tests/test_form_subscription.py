9c08394ca239e60462f9acedb76a28d03d7246dd
Levi Cruz <levicruz49@gmail.com>
2020-06-04 17:21:01 -0300

    Padroniza nomes

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -21,6 +21,12 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf='1234')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
+    def test_name_must_be_capitalized(self):
+        """ NAME MUST BE CAPITALIZED """
+        #LEVI cruz > Levi Cruz
+        form = self.make_validated_form(name='LEVI cruz')
+        self.assertEqual('Levi Cruz', form.cleaned_data['name'])
+
     def assertFormErrorCode(self, form, field, code):
         errors =  form.errors.as_data()
         errors_list = errors[field]
